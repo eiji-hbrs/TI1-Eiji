1d24de3435a5d7e3614809ce218e5395285f27ed
eiji-hbrs <eiji.haubursin@outlook.com>
2026-04-07 15:49:29 +0200

    rectification complète avant fin du premier jour 2

--- view/contact.php
+++ view/contact.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,51 +8,54 @@
     <link rel="stylesheet" href="./css/style.css">
 
 </head>
+
 <body>
     <header>
         <?php
-    include ROOT_PATH . "/view/inc/entete.php";
-    ?> 
- 
+        include ROOT_PATH . "/view/inc/entete.php";
+        ?>
+
         <?php
-    include ROOT_PATH . "/view/inc/menu.php";
-    ?>
+        include ROOT_PATH . "/view/inc/menu.php";
+        ?>
     </header>
-        <img src="../img/contacts.jpg" alt="contact">
+    <img src="../img/contacts.jpg" alt="contact">
 
     <main>
 
-       <h2>Pour nous contacter</h2>
+        <h2>Pour nous contacter</h2>
 
-        <p>Pour nous laisser un comentaire ou pour obtenir un renseignement complémentaire, remplissez le formulaire ci-dessous :</p>
+        <p>Pour nous laisser un comentaire ou pour obtenir un renseignement complémentaire, remplissez le formulaire
+            ci-dessous :</p>
 
         <form action="">
             <table>
-                    <tr>      
-                        <th><label for="nom">NOM :</label></th>
-                        <th><input type="text" name="nom" value="Entrez votre nom ..."></th>
-                    </tr>          
-                    <tr>    
-                        <th><label for="prenom">PRENOM :</label></th>
-                        <th><input type="text" name="prenom" value="Entrez votre prenom ..."></th>
-                    </tr>
-                    <tr>     
-                        <th><label for="ville">VILLE :</label></th>
-                        <th><input type="text" name="ville" value="Quelle est votre ville ..."></th>
-                    </tr> 
-                    <tr>   
-                        <th><label for="emai">E-MAIL :</label></th>
-                        <th><input type="email" name="email" value="Entrez votre email ..."></th>
-                    </tr> 
-                    <tr>          
-                        <th><label for="commentaire">COMMENTAIRES :</label></th>
-                        <th><input type="textarea" name="commentaire" value="Entrez votre commentaire ..."></th>
-                    </tr>
-                    <tr>
-                        <th><input type="submit" name="submit" value="Envoyer votre message"></th>
-                    </tr>
+                <tr>
+                    <th><label for="nom">NOM :</label></th>
+                    <th><input type="text" name="nom" value="Entrez votre nom ..."></th>
+                </tr>
+                <tr>
+                    <th><label for="prenom">PRENOM :</label></th>
+                    <th><input type="text" name="prenom" value="Entrez votre prenom ..."></th>
+                </tr>
+                <tr>
+                    <th><label for="ville">VILLE :</label></th>
+                    <th><input type="text" name="ville" value="Quelle est votre ville ..."></th>
+                </tr>
+                <tr>
+                    <th><label for="emai">E-MAIL :</label></th>
+                    <th><input type="email" name="email" value="Entrez votre email ..."></th>
+                </tr>
+                <tr>
+                    <th><label for="commentaire">COMMENTAIRES :</label></th>
+                    <th><input type="textarea" name="commentaire" value="Entrez votre commentaire ..."></th>
+                </tr>
+                <tr>
+                    <th><input type="submit" name="submit" value="Envoyer votre message"></th>
+                </tr>
             </table>
         </form>
     </main>
 </body>
+
 </html>
